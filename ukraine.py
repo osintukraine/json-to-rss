f797d12679f9ede0739c3e59969cca521e66161c
Ben Borges <info@buzzworkers.com>
2023-08-06 23:04:40 +0200

    Update ukraine.py

--- ukraine.py
+++ ukraine.py
@@ -1,10 +1,14 @@
-# convert a JSON live feed to xml
 import json
 import xml.etree.ElementTree as ET
 import requests
 import base64
 import re
+import os
+import unicodedata
 from urllib.parse import urlparse
+import logging
+from xml.dom import minidom
+import io
 
 def fetch_json_data(url):
     response = requests.get(url)
@@ -35,34 +39,44 @@ def decode_url(encoded_url):
 
     return decoded_url
 
-def dict_to_xml(dictionary, parent=None):
+def sanitize_value(value):
+    if isinstance(value, str):
+        # Remove non-printable characters from the string
+        sanitized_value = ''.join(char for char in value if unicodedata.category(char)[0] != 'C')
+        return sanitized_value
+    else:
+        return value
+
+def dict_to_xml(dictionary, parent=None, skip_keys=None):
     if parent is None:
         parent = ET.Element('root')
 
+    if skip_keys is None:
+        skip_keys = ['content_html', 'id']
+
     if isinstance(dictionary, dict):
         for key, value in dictionary.items():
-            if key == "content_html":  # Skip the "content_html" key
+            if key in skip_keys:
                 continue
 
             if isinstance(value, dict):
                 element = ET.SubElement(parent, key)
-                dict_to_xml(value, parent=element)
+                dict_to_xml(value, parent=element, skip_keys=skip_keys)
             elif isinstance(value, list):
                 for item in value:
                     element = ET.SubElement(parent, key)
-                    dict_to_xml(item, parent=element)
+                    dict_to_xml(item, parent=element, skip_keys=skip_keys)
             else:
+                element = ET.SubElement(parent, key)
                 if key == 'url':
                     decoded_url = decode_url(value)
-                    element = ET.SubElement(parent, key)
                     element.text = decoded_url
                 else:
-                    element = ET.SubElement(parent, key)
                     element.text = str(value)
     elif isinstance(dictionary, list):
         for item in dictionary:
             element = ET.SubElement(parent, 'item')
-            dict_to_xml(item, parent=element)
+            dict_to_xml(item, parent=element, skip_keys=skip_keys)
     else:
         parent.text = str(dictionary)
 
@@ -70,9 +84,42 @@ def dict_to_xml(dictionary, parent=None):
 
 
 def write_xml_to_file(xml_element, filename):
-    with open(filename, 'wb') as file:
-        tree = ET.ElementTree(xml_element)
-        tree.write(file, encoding='utf-8', xml_declaration=True)
+    # Update URLs with their decoded versions
+    update_decoded_urls(xml_element)
+
+    # Log the XML content before writing it to the file
+    xml_str = ET.tostring(xml_element, encoding='utf-8').decode('utf-8')
+    logging.info(f"XML content:\n{xml_str}")
+
+    if os.path.exists(filename):
+        # Load the existing XML file
+        try:
+            with open(filename, 'r') as existing_file:
+                existing_xml = existing_file.read()
+
+            # Clean up the existing XML file using minidom
+            parsed_xml = minidom.parseString(existing_xml)
+            cleaned_xml = parsed_xml.toprettyxml(indent="    ")
+
+            # Remove any invalid characters from the cleaned XML
+            cleaned_xml = ''.join(char for char in cleaned_xml if unicodedata.category(char)[0] != 'C')
+
+            # Update the existing XML with cleaned version
+            with open(filename, 'wb') as file:
+                file.write(cleaned_xml.encode('utf-8'))
+            print("XML file updated successfully.")
+        except Exception as e:
+            logging.error(f"Error parsing existing XML file: {e}")
+    else:
+        # If the file doesn't exist, write the entire XML
+        with io.BytesIO() as xml_buffer:
+            tree = ET.ElementTree(xml_element)
+            tree.write(xml_buffer, encoding='utf-8', xml_declaration=True)
+            with open(filename, 'wb') as file:
+                file.write(xml_buffer.getvalue())
+        print("Items appended successfully.")
+
+
 
 def update_decoded_urls(element):
     for child in element:
